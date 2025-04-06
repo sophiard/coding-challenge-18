1636d1b12981076ad17dc15e796d0b955c924ea2
Sophia Dolan <25srdolan@gmail.com>
2025-04-06 17:26:29 -0400

    Task 1: Create a New Component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,18 @@
+//Task 1 - Create a New Component
+
+
+import React from "react";
+//import react
+
+function Greeting() {
+    return (
+        <div>
+        <h2>Hello from the Greeting Component</h2>
+        <p>This is your first custom component in React.</p>
+        </div>
+    );
+} //created a new function greeting 
+
+
+export default Greeting;
+//export greeting 
